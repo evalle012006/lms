de32f8bfd3fda167abe9e757357d08d43bf0d53f
Rolando M. Evalle Jr <evalle012006@gmail.com>
2022-12-20 14:47:59 +0800

    Development (#41)
    * init
    
    * - added env file
    
    * - removed loans menu and transfer it under Transactions
    - added AddUpdateUserDrawer
    
    * - bug fixes
    - added role CRUD
    - added permissions CRUD
    
    * added roles and bug fixes
    
    * - added user management
    - bug fixes
    
    * - fixed display of branches when editting a user
    
    * - added groups management
    
    * - fixed delete in all modules
    - added client management
    - bug fixes
    
    * - added loans
    
    * - bug fixes
    - added roles for nav and data related
    - added details based on the last meeting
    
    * - bug fixes and enhancements
    
    * - added loan applications
    - bug fixes
    - permissions
    and many more...
    
    * - added client detail page
    - added modal
    - bug fixes
    
    * bug fixes
    
    * updates
    
    * updates
    
    * bug fixes
    
    * - updated the login screen
    
    * - bug fixes
    
    * Stopped tracking .env File
    
    * Stopped tracking next.config.js File
    
    * - added profile picture for clients
    
    * - bug fixes
    
    * - added daily cash collections
    - bug fixes
    
    * - fixed error in client list
    
    * - bug fixes
    
    * - bug fixes
    
    * - bug fixes
    
    * bug fixes
    
    * - updates
    
    * fixed issues
    
    * - updates
    
    * - updates
    
    * updates
    
    * updates
    
    * bug fixed
    
    * - updates
    
    * - fixed issue
    
    * - updates
    
    * - updates
    
    * - bug fix
    
    * - bug fixes
    
    * - bug fixes
    
    * - bug fix
    
    * - updates and bug fixes
    
    * - bug fix
    
    * - bug fixes
    
    * - remarks update
    
    * - updates
    
    * - updates
    
    * - fixes
    
    * - debugging
    
    * - removing debug
    
    * - bug fix
    
    * - bug fixed
    
    * - update query by branch/group
    
    * - page size fixes
    
    * - updates
    
    * - google analytics
    - fixed in onChange actual payment field
    
    * - debugging
    
    * - debugging
    
    * - updates
    - bug fixes
    
    * - fixes

--- src/pages/transactions/daily-cash-collection/client/[uuid].js
+++ src/pages/transactions/daily-cash-collection/client/[uuid].js
@@ -1060,7 +1060,6 @@ const CashCollectionDetailsPage = () => {
                     }
                 }
             }
-            console.log(cashCollection)
             cashCollection.sort((a, b) => { return a.slotNo - b.slotNo; });
 
             if (totalIdx > -1) {
@@ -1077,7 +1076,7 @@ const CashCollectionDetailsPage = () => {
         } else {
             for (let i = 1; i <= groupCapacity; i++) {
                 const existData = cashCollection.find(cc => cc.slotNo === i);
-
+                console.log(existData)
                 if (!existData) {
                     cashCollection.push({
                         slotNo: i,
@@ -1099,6 +1098,7 @@ const CashCollectionDetailsPage = () => {
                     });
                 } else {
                     const index = cashCollection.indexOf(existData);
+                    console.log(index);
                     cashCollection[index] = {
                         ...existData,
                         group: currentGroup
@@ -1113,7 +1113,7 @@ const CashCollectionDetailsPage = () => {
             } else {
                 cashCollection.push(calculateTotals(cashCollection));
             }
-
+            console.log(cashCollection)
             // setTimeout(() => {
             //     dispatch(setCashCollectionGroup(cashCollection));
             //     setLoading(false);
