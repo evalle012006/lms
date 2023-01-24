2829796942ac5a6c3ad388af7308e5c50d681f05
evalle012006 <evalle012006@gmail.com>
2023-01-24 21:56:45 +0800

    - debugging

--- src/pages/transactions/daily-cash-collection/index.js
+++ src/pages/transactions/daily-cash-collection/index.js
@@ -37,6 +37,7 @@ const DailyCashCollectionPage = () => {
 
     const handleSubmitForLos = async () => {
         setLoading(true);
+        console.log(cashCollectionList)
         if (cashCollectionList.length > 0) {
             const filteredGroups = cashCollectionList.filter(cc => cc.status !== '-').map(cc => { return cc.groupId });
 
@@ -93,7 +94,7 @@ const DailyCashCollectionPage = () => {
     }, [currentUser]);
 
     useEffect(() => {
-        if (branchList.length > 0) {
+        if (branchList) {
             localStorage.setItem('cashCollectionDateFilter', currentDate);
 
             if (currentUser.role.rep < 4) {
