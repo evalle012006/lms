41f61d38abf391ecf4c362775745332b87a62221
evalle012006 <evalle012006@gmail.com>
2023-04-24 15:01:18 +0800

    - fixed pre-save collection not saving for weekly transactions

--- src/pages/api/transactions/cash-collections/pre-save-collections.js
+++ src/pages/api/transactions/cash-collections/pre-save-collections.js
@@ -35,51 +35,47 @@ async function save(req, res) {
         const existCC = await db.collection('cashCollections').find({ loanId: loan._id + '', dateAdded: currentDate }).toArray();
 
         if (existCC.length === 0) {
-            const groupSummary = await db.collection('groupCashCollections').find({ groupId: loan.groupId, dateAdded: currentDate }).toArray();
             const group = await db.collection('groups').find({ _id: new ObjectId(loan.groupId) }).toArray();
-            if (groupSummary.length > 0) {
-                let data = {
-                    loanId: loan._id + '',
-                    branchId: loan.branchId,
-                    groupId: loan.groupId,
-                    groupname: loan.groupName,
-                    loId: loan.loId,
-                    clientId: loan.clientId,
-                    slotNo: loan.slotNo,
-                    loanCycle: loan.loanCycle,
-                    mispayment: false,
-                    mispaymentStr: 'No',
-                    collection: 0,
-                    excess: 0,
-                    total: 0,
-                    noOfPayments: 0,
-                    activeLoan: loan.activeLoan,
-                    targetCollection: loan.activeLoan, 
-                    amountRelease: loan.amountRelease,
-                    loanBalance: loan.loanBalance,
-                    paymentCollection: 0,
-                    occurence: loan.occurence,
-                    currentReleaseAmount: 0,
-                    fullPayment: 0,
-                    mcbu: loan.mcbu,
-                    mcbuCol: 0,
-                    mcbuWithdrawal: 0,
-                    mcbuReturnAmt: 0,
-                    remarks: '',
-                    status: loan.status,
-                    dateAdded: currentDate,
-                    groupCollectionId: groupSummary[0]._id + '',
-                    groupStatus: "pending",
-                    origin: 'pre-save'
-                };
+            let data = {
+                loanId: loan._id + '',
+                branchId: loan.branchId,
+                groupId: loan.groupId,
+                groupname: loan.groupName,
+                loId: loan.loId,
+                clientId: loan.clientId,
+                slotNo: loan.slotNo,
+                loanCycle: loan.loanCycle,
+                mispayment: false,
+                mispaymentStr: 'No',
+                collection: 0,
+                excess: 0,
+                total: 0,
+                noOfPayments: 0,
+                activeLoan: loan.activeLoan,
+                targetCollection: loan.activeLoan, 
+                amountRelease: loan.amountRelease,
+                loanBalance: loan.loanBalance,
+                paymentCollection: 0,
+                occurence: loan.occurence,
+                currentReleaseAmount: 0,
+                fullPayment: 0,
+                mcbu: loan.mcbu,
+                mcbuCol: 0,
+                mcbuWithdrawal: 0,
+                mcbuReturnAmt: 0,
+                remarks: '',
+                status: loan.status,
+                dateAdded: currentDate,
+                groupStatus: "pending",
+                origin: 'pre-save'
+            };
 
-                if (loan.occurence === 'weekly') {
-                    data.mcbuTarget = 50;
-                    data.groupDay = group[0].day;
-                }
-    
-                await db.collection('cashCollections').insertOne({ ...data });
+            if (loan.occurence === 'weekly') {
+                data.mcbuTarget = 50;
+                data.groupDay = group[0].day;
             }
+
+            await db.collection('cashCollections').insertOne({ ...data });
         }
     });
 
