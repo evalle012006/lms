b863901cd1e1b935448724a9f458c3577cd8179b
Rhey Evalle <rheyandreane.evalle@gmail.com>
2023-07-15 12:38:52 +0800

    - added sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from "@/components/Layout";
 import { fetchWrapper } from '@/lib/fetch-wrapper';
 import { setSystemSettings } from '@/redux/actions/systemActions';
 import moment from 'moment';
-import { useState } from 'react';
 
 const Index = () => {
     const dispatch = useDispatch();
