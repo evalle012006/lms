2b4ad2cd569f7410a5b66b5e1aa2a7fb084499ed
evalle012006 <evalle012006@gmail.com>
2023-09-11 12:01:55 +0800

    - fixed loan approval issue

--- src/pages/api/transactions/loans/approved-reject-by-batch.js
+++ src/pages/api/transactions/loans/approved-reject-by-batch.js
@@ -138,8 +138,8 @@ async function getCoMakerInfo(coMaker, groupId) {
     let client;
     if (typeof coMaker === 'number') {
         const loan = await db.collection('loans').aggregate([ 
-            {$match: 
-                { $and: [
+            { $match: {
+                $expr: { $and: [
                     { $eq: ['$groupId', groupId] },
                     { $or: [
                         {$eq: ['$status', 'active']},
@@ -147,7 +147,7 @@ async function getCoMakerInfo(coMaker, groupId) {
                     ] },
                     { $eq: ['$slotNo', coMaker] }
                 ] }
-            } 
+            } } 
         ]).toArray();
 
         if (loan) {
@@ -155,15 +155,15 @@ async function getCoMakerInfo(coMaker, groupId) {
         }
     } else if (typeof coMaker === 'string') {
         const loan = await db.collection('loans').aggregate([ 
-            {$match: 
-                { $and: [
+            { $match: {
+                $expr: { $and: [
                     { $eq: ['$clientId', coMaker] },
                     { $or: [
                         {$eq: ['$status', 'active']},
                         {$eq: ['$status', 'pending']}
                     ] }
                 ] }
-            } 
+            } } 
         ]).toArray();
 
         if (loan) {
