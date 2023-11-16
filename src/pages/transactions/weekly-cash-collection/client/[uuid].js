46a629d5e4c0528c241e13becdf8c3b55c5ec892
evalle012006 <evalle012006@gmail.com>
2023-11-16 09:12:12 +0800

    - fixed loan balance not reset when setting delinquent remarks

--- src/pages/transactions/weekly-cash-collection/client/[uuid].js
+++ src/pages/transactions/weekly-cash-collection/client/[uuid].js
@@ -1684,6 +1684,10 @@ const CashCollectionDetailsPage = () => {
                                         toast.error("Error occured. Remarks is not valid due to the amount in Actual Collection.");
                                     } else {
                                         if (temp.remarks.value == 'delinquent') {
+                                            if (temp.paymentCollection > 0) {
+                                                temp.loanBalance += temp.paymentCollection;
+                                                temp.loanBalanceStr = formatPricePhp(temp.loanBalance);
+                                            }
                                             temp.paymentCollection = 0;
                                             temp.paymentCollectionStr = '-';
                                         }
