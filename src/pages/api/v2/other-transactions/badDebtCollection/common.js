bb2b9215c5549ca2da7bc64b6f8bcee9cac63ab6
jayrome <jaycverg@gmail.com>
2024-04-12 21:44:19 +0800

    WIP: badDebtCollections

--- src/pages/api/v2/other-transactions/badDebtCollection/common.js
+++ src/pages/api/v2/other-transactions/badDebtCollection/common.js
@@ -1,7 +1,5 @@
 import { createGraphType } from "@/lib/graph/graph.util";
 import { BAD_DEBT_COLLECTIONS_FIELDS } from "@/lib/graph.fields";
 
-export const badDebtCollectionsType = createGraphType(
-  "badDebtCollections",
-  BAD_DEBT_COLLECTIONS_FIELDS
-)();
+export const createBadDebtCollectionsType = (fields = BAD_DEBT_COLLECTIONS_FIELDS) =>
+  createGraphType("badDebtCollections", fields)();
