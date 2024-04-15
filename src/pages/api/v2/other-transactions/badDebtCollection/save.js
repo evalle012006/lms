2275999223ba5add5b784e1c7b47e7944881b6c7
jayrome <jaycverg@gmail.com>
2024-04-15 22:43:08 +0800

    WIP: badDebtCollections/save

--- src/pages/api/v2/other-transactions/badDebtCollection/save.js
+++ src/pages/api/v2/other-transactions/badDebtCollection/save.js
@@ -1,59 +1,73 @@
-import { apiHandler } from '@/services/api-handler';
-import { connectToDatabase } from '@/lib/mongodb';
-import { getCurrentDate } from '@/lib/utils';
-import moment from 'moment'
+import { apiHandler } from "@/services/api-handler";
+import { getCurrentDate } from "@/lib/utils";
+import moment from "moment";
+import { GraphProvider } from "@/lib/graph/graph.provider";
+import { createGraphType, insertQl, queryQl, updateQl } from '@/lib/graph/graph.util'
+import { LOAN_FIELDS } from '@/lib/graph.fields'
+import { createBadDebtCollectionsType } from '@/pages/api/v2/other-transactions/badDebtCollection/common'
+
+const graph = new GraphProvider();
+const loanType = createGraphType("loans", LOAN_FIELDS)();
 
 export default apiHandler({
-    post: save
+  post: save,
 });
 
 async function save(req, res) {
-    const { db } = await connectToDatabase();
-    const ObjectId = require('mongodb').ObjectId;
+  const formData = req.body;
+  const loan = await findMaturedLoan(formData.loanId);
 
-    let response = {};
-    let statusCode = 200;
-
-    const formData = req.body;
-
-    const maturedLoan = await db.collection('loans').find({ _id: new ObjectId(formData.loanId), maturedPD: true }).toArray();
-
-    if (maturedLoan.length > 0) {
-        let loan = maturedLoan[0];
-        const maturedPD = formData.maturedPastDue - formData.paymentCollection;
-        loan.maturedPastDue = maturedPD;
-        if (loan.hasOwnProperty('noBadDebtPayment')) {
-            loan.noBadDebtPayment += 1;
-        } else {
-            loan.noBadDebtPayment = 1;
-        }
-
-        if (maturedPD <= 0) {
-            loan.maturedPastDue = 0;
-        }
-
-        delete loan._id;
-
-        await db.collection('loans').updateOne({ _id: new ObjectId(formData.loanId) }, { $set: { ...loan } });
-
-        const data = await db.collection('badDebtCollections').insertOne({
-            ...formData,
-            maturedPastDue: maturedPD,
-            dateAdded: moment(getCurrentDate()).format('YYYY-MM-DD')
-        });
-    
-        response = {
-            success: true,
-            data: data
-        }
+  if (loan) {
+    const maturedPD = formData.maturedPastDue - formData.paymentCollection;
+    loan.maturedPastDue = maturedPD;
+    if (loan.noBadDebtPayment) {
+      loan.noBadDebtPayment += 1;
     } else {
-        response = {
-            error: true,
-            message: 'No matured loan detected for this client!'
-        }
+      loan.noBadDebtPayment = 1;
     }
 
-    res.status(statusCode)
-        .setHeader('Content-Type', 'application/json')
-        .end(JSON.stringify(response));
+    if (maturedPD <= 0) {
+      loan.maturedPastDue = 0;
+    }
+
+    delete loan._id;
+
+    const operations = [
+      updateQl(loanType, {
+        where: { _id: { _eq: formData.loanId } },
+        set: loan,
+      }),
+      insertQl(createBadDebtCollectionsType(), {
+        objects: [{
+          ...formData,
+          maturedPastDue: maturedPD,
+          dateAdded: moment(getCurrentDate()).format("YYYY-MM-DD"),
+        }]
+      })
+    ];
+
+    const { data } = await graph.mutation(...operations);
+
+    res.send({
+      success: true,
+      data: data?.badDebtCollections?.returning?.[0],
+    });
+  } else {
+    res.send({
+      error: true,
+      message: "No matured loan detected for this client!",
+    });
+  }
+}
+
+async function findMaturedLoan(loanId) {
+  const res = await graph.query(
+    queryQl(loanType, {
+      where: {
+        _id: { _eq: loanId },
+        maturedPD: { _eq: true },
+      },
+    })
+  );
+  return res.data?.loans?.[0];
 }
