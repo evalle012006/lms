f6a67994d683e020d6971ebdb226dd3ecc389aa7
ramoscmichael <34493051+ramoscmichael@users.noreply.github.com>
2024-04-17 23:59:36 +0800

    ad v2 api for reports/get-all-branch-not-close

--- src/pages/api/v2/reports/get-all-branch-not-close.js
+++ src/pages/api/v2/reports/get-all-branch-not-close.js
@@ -1,5 +1,28 @@
 import { apiHandler } from '@/services/api-handler';
 import { connectToDatabase } from '@/lib/mongodb';
+import { GraphProvider } from '@/lib/graph/graph.provider';
+import { createGraphType, queryQl } from '@/lib/graph/graph.util';
+import { BRANCH_FIELDS } from '@/lib/graph.fields';
+
+const graph = new GraphProvider();
+const CASH_COLLECTION_TYPE = createGraphType('cashCollections', `
+branchId,
+loanId,
+groupId
+groupName
+loanOfficer {
+    firstName
+    lastName
+}
+clientId
+fullName
+branch {
+    name
+}
+slotNo
+dateAdded
+${fields.join('\n')}
+`)('cashCollections');
 
 export default apiHandler({
     get: allLoans
@@ -14,342 +37,95 @@ async function allLoans(req, res) {
 
     let cashCollectionsDraft;
     let cashCollectionsPending;
-
+    
     if (loId) {
-        cashCollectionsDraft = await db.collection('cashCollections')
-            .aggregate([
-                { $match: { draft: true, loId: loId } },
-                { $addFields: {
-                    branchId: { $toObjectId: '$branchId' },
-                    loId: { $toObjectId: '$loId' }
-                } },
-                {
-                    $lookup: {
-                        from: "branches",
-                        localField: "branchId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { name: '$name' } }
-                        ],
-                        as: "branch"
-                    }
-                },
-                {
-                    $unwind: '$branch'
-                },
-                {
-                    $lookup: {
-                        from: "users",
-                        localField: "loId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                        ],
-                        as: "loanOfficer"
-                    }
-                },
-                {
-                    $unwind: '$loanOfficer'
-                },
-                { $project: {
-                    branchName: '$branch.name',
-                    loanOfficerFirstName: '$loanOfficer.firstName',
-                    loanOfficerLastName: '$loanOfficer.lastName',
-                    groupId: '$groupId',
-                    groupName: '$groupName',
-                    clientId: '$clientId',
-                    clientName: '$fullName',
-                    slotNo: '$slotNo',
-                    loanId: '$loanId',
-                    dateAdded: '$dateAdded'
-                } }
-            ])
-            .toArray();
 
-        cashCollectionsPending = await db.collection('cashCollections')
-            .aggregate([
-                { $match: { groupStatus: 'pending', loId: loId } },
-                { $addFields: {
-                    branchId: { $toObjectId: '$branchId' },
-                    loId: { $toObjectId: '$loId' }
-                } },
-                {
-                    $lookup: {
-                        from: "branches",
-                        localField: "branchId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { name: '$name' } }
-                        ],
-                        as: "branch"
-                    }
-                },
-                {
-                    $unwind: '$branch'
-                },
-                {
-                    $lookup: {
-                        from: "users",
-                        localField: "loId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                        ],
-                        as: "loanOfficer"
-                    }
-                },
-                {
-                    $unwind: '$loanOfficer'
-                },
-                { $project: {
-                    branchName: '$branch.name',
-                    loanOfficerFirstName: '$loanOfficer.firstName',
-                    loanOfficerLastName: '$loanOfficer.lastName',
-                    groupId: '$groupId',
-                    groupName: '$groupName',
-                    clientId: '$clientId',
-                    clientName: '$fullName',
-                    slotNo: '$slotNo',
-                    loanId: '$loanId',
-                    dateAdded: '$dateAdded'
-                } }
-            ])
-            .toArray();
+        cashCollectionsDraft = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    loId: { _eq: loId },
+                    draft: { _eq: true }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
+
+        cashCollectionsPending = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    loId: { _eq: loId },
+                    groupStatus: { _eq: 'peding' }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
+
     } else if (branchId || branchIds) {
-        if (branchId) {
-            cashCollectionsDraft = await getByBranchDraft(branchId);
-            cashCollectionsPending = await getByBranchPending(branchId);
-        } else {
-            const branchIdsObj = JSON.parse(branchIds);
-            const draftData = [];
-            const pendingData = [];
-            const promise = await new Promise(async (resolve) => {
-                const response = await Promise.all(branchIdsObj.map(async (branchId) => {
-                    draftData.push.apply(draftData, await getByBranchDraft(branchId));
-                    pendingData.push.apply(pendingData, await getByBranchPending(branchId));
-                }));
+        const branche_ids = [branchId, ... (branchIds ?? [])].filter(b => !!b);
+        
+        cashCollectionsDraft = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    branchId: { _in: branche_ids },
+                    draft: { _eq: true }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
 
-                resolve(response);
-            });
-
-            if (promise) {
-                cashCollectionsDraft = draftData;
-                cashCollectionsPending = pendingData;
-            }
-        }
+        cashCollectionsPending = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    branchId: { _in: branche_ids },
+                    groupStatus: { _eq: 'peding' }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
     } else {
-        cashCollectionsDraft = await db.collection('cashCollections')
-            .aggregate([
-                { $match: { draft: true } },
-                { $addFields: {
-                    branchId: { $toObjectId: '$branchId' },
-                    loId: { $toObjectId: '$loId' }
-                } },
-                {
-                    $lookup: {
-                        from: "branches",
-                        localField: "branchId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { name: '$name' } }
-                        ],
-                        as: "branch"
-                    }
-                },
-                {
-                    $unwind: '$branch'
-                },
-                {
-                    $lookup: {
-                        from: "users",
-                        localField: "loId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                        ],
-                        as: "loanOfficer"
-                    }
-                },
-                {
-                    $unwind: '$loanOfficer'
-                },
-                { $project: {
-                    branchName: '$branch.name',
-                    loanOfficerFirstName: '$loanOfficer.firstName',
-                    loanOfficerLastName: '$loanOfficer.lastName',
-                    groupId: '$groupId',
-                    groupName: '$groupName',
-                    clientId: '$clientId',
-                    clientName: '$fullName',
-                    slotNo: '$slotNo',
-                    loanId: '$loanId',
-                    dateAdded: '$dateAdded'
-                } }
-            ])
-            .toArray();
 
-        cashCollectionsPending = await db.collection('cashCollections')
-            .aggregate([
-                { $match: { groupStatus: 'pending' } },
-                { $addFields: {
-                    branchId: { $toObjectId: '$branchId' },
-                    loId: { $toObjectId: '$loId' }
-                } },
-                {
-                    $lookup: {
-                        from: "branches",
-                        localField: "branchId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { name: '$name' } }
-                        ],
-                        as: "branch"
-                    }
-                },
-                {
-                    $unwind: '$branch'
-                },
-                {
-                    $lookup: {
-                        from: "users",
-                        localField: "loId",
-                        foreignField: "_id",
-                        pipeline: [
-                            { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                        ],
-                        as: "loanOfficer"
-                    }
-                },
-                {
-                    $unwind: '$loanOfficer'
-                },
-                { $project: {
-                    branchName: '$branch.name',
-                    loanOfficerFirstName: '$loanOfficer.firstName',
-                    loanOfficerLastName: '$loanOfficer.lastName',
-                    groupId: '$groupId',
-                    groupName: '$groupName',
-                    clientId: '$clientId',
-                    clientName: '$fullName',
-                    slotNo: '$slotNo',
-                    loanId: '$loanId',
-                    dateAdded: '$dateAdded'
-                } }
-            ])
-            .toArray();
+        cashCollectionsDraft = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    draft: { _eq: true }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
+
+        cashCollectionsPending = await graph.query(
+            queryQl(CASH_COLLECTION_TYPE, {
+                where: {
+                    groupStatus: { _eq: 'peding' }
+                }
+            })
+        ).then(res => res.data.cashCollections ?? []);
     }
+
+    cashCollectionsDraft = cashCollectionsDraft.map(c => ({
+        branchName: c.branch.name,
+        loanOfficerFirstName: c.loanOfficer.firstName,
+        loanOfficerLastName: c.loanOfficer.lastName,
+        groupId: c.groupId,
+        groupName: c.groupName,
+        clientId: c.clientId,
+        clientName: c.fullName,
+        slotNo: c.slotNo,
+        loanId: c.loanId,
+        dateAdded: c.dateAdded,
+     }));
+
+     cashCollectionsPending = cashCollectionsPending.map(c => ({
+        branchName: c.branch.name,
+        loanOfficerFirstName: c.loanOfficer.firstName,
+        loanOfficerLastName: c.loanOfficer.lastName,
+        groupId: c.groupId,
+        groupName: c.groupName,
+        clientId: c.clientId,
+        clientName: c.fullName,
+        slotNo: c.slotNo,
+        loanId: c.loanId,
+        dateAdded: c.dateAdded,
+     }));
 
     response = { success: true, cashCollectionsDraft: cashCollectionsDraft, cashCollectionsPending: cashCollectionsPending };
 
     res.status(statusCode)
         .setHeader('Content-Type', 'application/json')
         .end(JSON.stringify(response));
-}
-
-const getByBranchDraft = async (branchId) => {
-    const { db } = await connectToDatabase();
-
-    return await db.collection('cashCollections')
-        .aggregate([
-            { $match: { draft: true, branchId: branchId } },
-            { $addFields: {
-                branchId: { $toObjectId: '$branchId' },
-                loId: { $toObjectId: '$loId' }
-            } },
-            {
-                $lookup: {
-                    from: "branches",
-                    localField: "branchId",
-                    foreignField: "_id",
-                    pipeline: [
-                        { $project: { name: '$name' } }
-                    ],
-                    as: "branch"
-                }
-            },
-            {
-                $unwind: '$branch'
-            },
-            {
-                $lookup: {
-                    from: "users",
-                    localField: "loId",
-                    foreignField: "_id",
-                    pipeline: [
-                        { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                    ],
-                    as: "loanOfficer"
-                }
-            },
-            {
-                $unwind: '$loanOfficer'
-            },
-            { $project: {
-                branchName: '$branch.name',
-                loanOfficerFirstName: '$loanOfficer.firstName',
-                loanOfficerLastName: '$loanOfficer.lastName',
-                groupId: '$groupId',
-                groupName: '$groupName',
-                clientId: '$clientId',
-                clientName: '$fullName',
-                slotNo: '$slotNo',
-                loanId: '$loanId',
-                dateAdded: '$dateAdded'
-            } }
-        ])
-        .toArray();
-}
-
-const getByBranchPending = async (branchId) => {
-    const { db } = await connectToDatabase();
-
-    return await db.collection('cashCollections')
-        .aggregate([
-            { $match: { groupStatus: 'pending', branchId: branchId } },
-            { $addFields: {
-                branchId: { $toObjectId: '$branchId' },
-                loId: { $toObjectId: '$loId' }
-            } },
-            {
-                $lookup: {
-                    from: "branches",
-                    localField: "branchId",
-                    foreignField: "_id",
-                    pipeline: [
-                        { $project: { name: '$name' } }
-                    ],
-                    as: "branch"
-                }
-            },
-            {
-                $unwind: '$branch'
-            },
-            {
-                $lookup: {
-                    from: "users",
-                    localField: "loId",
-                    foreignField: "_id",
-                    pipeline: [
-                        { $project: { firstName: '$firstName', lastName: '$lastName' } }
-                    ],
-                    as: "loanOfficer"
-                }
-            },
-            {
-                $unwind: '$loanOfficer'
-            },
-            { $project: {
-                branchName: '$branch.name',
-                loanOfficerFirstName: '$loanOfficer.firstName',
-                loanOfficerLastName: '$loanOfficer.lastName',
-                groupId: '$groupId',
-                groupName: '$groupName',
-                clientId: '$clientId',
-                clientName: '$fullName',
-                slotNo: '$slotNo',
-                loanId: '$loanId',
-                dateAdded: '$dateAdded'
-            } }
-        ])
-        .toArray();
 }
