3a2be0158b424bd34458620427870a63e65bce6a
ramoscmichael <34493051+ramoscmichael@users.noreply.github.com>
2024-05-12 11:26:50 +0800

    add get loan group per loan officer

--- src/pages/api/v2/transactions/cash-collections/get-all-loans-per-lo-v2.js
+++ src/pages/api/v2/transactions/cash-collections/get-all-loans-per-lo-v2.js
@@ -1,32 +1,33 @@
 import { apiHandler } from '@/services/api-handler';
-import moment from  'moment';
-
-const graph = new GraphProvider();
+import moment from 'moment';
+import logger from '@/logger';
 
 export default apiHandler({
     get: getData
 });
 
 async function getData(req, res) {
-    const { date, mode, groupIds, dayName, currentDate } = req.query;
+    const { date, mode, loIds, dayName, currentDate } = req.query;
     let statusCode = 200;
     let response = {};
 
-    const groupIdsObj = JSON.parse(groupIds);
+    const loIdsObj = JSON.parse(loIds);
     const data = [];
 
     const promise = await new Promise(async (resolve) => {
-        const response = await Promise.all(groupIdsObj.map(async (groupId) => {
-            data.push.apply(data, await getAllLoansPerGroup(date, mode, groupId, dayName, currentDate));
+        const response = await Promise.all(loIdsObj.map(async (loId) => {
+            logger.debug({page: 'Loan Officer Collections', message: `Getting data for loan officer id: ${loId}`});
+            data.push.apply(data, await getAllLoansPerGroup(date, mode, loId, dayName, currentDate));
         }));
 
         resolve(response);
     });
 
     if (promise) {
-        data.sort((a,b) => { return a.groupNo - b.groupNo });
+        data.sort((a, b) => { return a.loNo - b.loNo });
         response = { success: true, data: data };
-    } else {
+    }
+    else {
         statusCode = 500;
         response = { error: true, message: "Error fetching data" };
     }
@@ -36,21 +37,20 @@ async function getData(req, res) {
         .end(JSON.stringify(response));
 }
 
-async function getAllLoansPerGroup(date, mode, groupId, dayName, currentDate) {
-
+async function getAllLoansPerGroup(date, mode, loId, dayName, currentDate) {
     let cashCollection;
 
     if (currentDate === date) {
       dayName = moment(date).format('dddd').toLowerCase();
       cashCollection = await graph.apollo.query({
           query: gql`
-          query loan_group ($day_name: String!, $curr_date: date!, $groupIds: [String!]!) {
-              collections: get_all_loans_per_group_by_curr_date_and_day_name(args: {
+          query loan_group ($day_name: String!, $curr_date: date!, $loIds: [String!]!) {
+              collections: get_all_loans_per_lo_by_curr_date_and_day_name(args: {
                 day_name: $day_name,
                 curr_date: $curr_date
               }, where: {
                 _id: {
-                  _in: $groupIds
+                  _in: $loIds
                 }
               }) {
                 _id
@@ -61,19 +61,19 @@ async function getAllLoansPerGroup(date, mode, groupId, dayName, currentDate) {
           variables: {
               day_name: dayName,
               curr_date: date,
-              groupIds: [groupId]
+              loIds: [loId]
           }
       }).then(res => res.collections.map(c => c.data));
     } else {
         cashCollection = await graph.apollo.query({
             query: gql`
-            query loan_group ($day_name: String!, $date_added: date!, $groupIds: [String!]!) {
-                collections: get_all_loans_per_group_by_date_added_and_day_name(args: {
+            query loan_group ($day_name: String!, $date_added: date!, $loIds: [String!]!) {
+                collections: get_all_loans_per_lo_by_date_added_and_day_name(args: {
                   day_name: $day_name,
                   date_added: $date_added
                 }, where: {
                   _id: {
-                    _in: $groupIds
+                    _in: $loIds
                   }
                 }) {
                   _id
@@ -84,7 +84,7 @@ async function getAllLoansPerGroup(date, mode, groupId, dayName, currentDate) {
             variables: {
                 day_name: dayName,
                 date_added: date,
-                groupIds: [groupId]
+                loIds: [loId],
             }
         }).then(res => res.collections.map(c => c.data));
     }
