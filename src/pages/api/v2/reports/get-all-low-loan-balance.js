cbb2d5262b6d2d31e27e309be793768a825c349a
ramoscmichael <34493051+ramoscmichael@users.noreply.github.com>
2024-05-25 00:51:16 +0800

    update get all low loan balance

--- src/pages/api/v2/reports/get-all-low-loan-balance.js
+++ src/pages/api/v2/reports/get-all-low-loan-balance.js
@@ -3,6 +3,7 @@ import { connectToDatabase } from '@/lib/mongodb';
 import { GraphProvider } from '@/lib/graph/graph.provider';
 import { gql } from 'node_modules/apollo-boost/lib/index';
 import { createGraphType, queryQl } from '@/lib/graph/graph.util';
+import { USER_FIELDS } from '@/lib/graph.fields';
 
 const graph = new GraphProvider();
 
@@ -21,6 +22,7 @@ query groups ($groupWhere: groups_bool_exp, $loanWhere: loans_bool_exp){
           loanCycle
           mcbu
           noOfMisPayments: mispayment
+          noOfPayments
           clientStatus: client {
               delinquent
           }
@@ -81,8 +83,11 @@ async function allLoans(req, res) {
     let response;
     let statusCode = 200;
 
-    const { loId, branchIds, branchId, amount, operator } = req.query;
+    const { loId, currentUserId, branchId, amountOption, noOfPaymentsOption } = req.query;
     let data = [];
+
+    const amountOptionObj = JSON.parse(amountOption);
+    const noOfPaymentsOptionObj = JSON.parse(noOfPaymentsOption);
 
     const operator_key = {
         less_than_equal: '_lte',
@@ -97,10 +102,18 @@ async function allLoans(req, res) {
                     _eq: 'active'
                 }
             },
-            ... [operator_key[operator]].filter(_key => !!_key)
+
+            ... [operator_key[amountOptionObj.operator]].filter(_key => !!_key)
                             .map(_key => ({
                                 loanBalance: {
-                                    [_key]: parseInt(amount)
+                                    [_key]: parseInt(amountOptionObj.amount)
+                                }
+                            })),
+
+            ... [operator_key[noOfPaymentsOptionObj.operator]].filter(_key => !!_key)
+                            .map(_key => ({
+                                noOfPayments: {
+                                    [_key]: parseInt(noOfPaymentsOptionObj.amount)
                                 }
                             }))
         ]
@@ -117,23 +130,42 @@ async function allLoans(req, res) {
                 loanWhere,
             }
         }).then(r => r.data.groups ?? []);
-        
+
     }  else if (branchId) {
         const userIds = await graph.query(queryQl(createGraphType('users', `_id`)('users'), { 
             where: { role: { _contains: { rep: 4 } } },
             order_by: [{ loNo: 'asc' }],
         })).then(res => res.data.users.map(u => u._id));
 
-        data = await Promise.all(userIds.map(async _id => await getQueryById(USERS_QL, branchId, loanWhere)));
+        data = await Promise.all(userIds.map(async _id => await getQueryById(USERS_QL, _id, {
+            loId: { _eq: _id },
+            ... loanWhere
+        })));
     } else {
-        let branchIdsObj;
-        if (branchIds) {
-            branchIdsObj = JSON.parse(branchIds);
+        let branchIdsObj = [];
+
+        if(currentUserId) {
+            const user = await graph.query(queryQl(createGraphType('users', `${USER_FIELDS}`), { where: { _id: { _eq: currentUserId } }})).then(res => res.data.users);
+            if (user.length > 0) {
+                if (user[0].areaId && user[0].role.shortCode === 'area_admin') {
+                    const branches = await db.collection('branches').find({ areaId: user[0].areaId }).toArray();
+                    branchIdsObj = branches.map(branch => branch._id.toString());
+                } else if (user[0].regionId && user[0].role.shortCode === 'regional_manager') {
+                    const branches = await db.collection('branches').find({ regionId: user[0].regionId }).toArray();
+                    branchIdsObj = branches.map(branch => branch._id.toString());
+                } else if (user[0].divisionId && user[0].role.shortCode === 'deputy_director') {
+                    const branches = await db.collection('branches').find({ divisionId: user[0].divisionId }).toArray();
+                    branchIdsObj = branches.map(branch => branch._id.toString());
+                }
+            }
         } else {
             branchIdsObj = await graph.query(queryQl(createGraphType('branches', '_id')('branches'))).then(res => res.data.branches.map(b => b._id));
         }
 
-        data = await Promise.all(branchIdsObj.map(async _id => await getQueryById(BRANCH_QL, branchId, loanWhere)));
+        data = await Promise.all(branchIdsObj.map(async _id => await getQueryById(BRANCH_QL, _id, {
+            branchId: { _eq: _id },
+            ... loanWhere
+        })));
     }
 
     response = { success: true, data: data };
