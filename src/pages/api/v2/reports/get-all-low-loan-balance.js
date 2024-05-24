611209c8a2f0f3186b9bd81eb71251cdc40df6e9
ramoscmichael <34493051+ramoscmichael@users.noreply.github.com>
2024-05-24 21:17:27 +0800

    add get all low loan balance;

--- src/pages/api/v2/reports/get-all-low-loan-balance.js
+++ src/pages/api/v2/reports/get-all-low-loan-balance.js
@@ -1,5 +1,76 @@
 import { apiHandler } from '@/services/api-handler';
 import { connectToDatabase } from '@/lib/mongodb';
+import { GraphProvider } from '@/lib/graph/graph.provider';
+import { gql } from 'node_modules/apollo-boost/lib/index';
+import { createGraphType, queryQl } from '@/lib/graph/graph.util';
+
+const graph = new GraphProvider();
+
+const GROUP_QL = gql`
+query groups ($groupWhere: groups_bool_exp, $loanWhere: loans_bool_exp){
+    groups (where: $groupWhere){
+      _id
+      name
+      branchId
+      loans (where: $loanWhere) {
+          clientId
+          clientName: fullName
+          slotNo
+          loanBalance
+          amountRelease
+          loanCycle
+          mcbu
+          noOfMisPayments: mispayment
+          clientStatus: client {
+              delinquent
+          }
+      }
+    }
+}
+`;
+
+const USERS_QL = gql`
+query users ($_id: String, $loanWhere: loans_bool_exp){
+    user: users (where: {
+      _id: { _eq: $_id }
+    }){
+      _id
+      firstName
+      lastName
+      branchId: designatedBranchId
+    }
+    
+    loans: loans_aggregate (where: $loanWhere) {
+      aggregate {
+        totalClients: count
+        totalAmountRelease: sum {amountRelease }
+        totalLoanBalance: sum { loanBalance }
+        totalMCBU: sum { mcbu }
+      }
+    }
+  }
+`;
+
+const BRANCH_QL = gql`
+query branches ($_id: String, $loanWhere: loans_bool_exp){
+    branch: branches (where: {
+      _id: { _eq: $_id }
+    }){
+      _id
+      name
+      code
+    }
+    
+    loans: loans_aggregate (where: $loanWhere) {
+      aggregate {
+        totalClients: count
+        totalAmountRelease: sum {amountRelease }
+        totalLoanBalance: sum { loanBalance }
+        totalMCBU: sum { mcbu }
+      }
+    }
+}
+`;
 
 export default apiHandler({
     get: allLoans
@@ -13,163 +84,56 @@ async function allLoans(req, res) {
     const { loId, branchIds, branchId, amount, operator } = req.query;
     let data = [];
 
+    const operator_key = {
+        less_than_equal: '_lte',
+        greater_than_equal: '_gte',
+        equal: '_eq'
+    };
+
+    const loanWhere = {
+        _and: [
+            {
+                status: { 
+                    _eq: 'active'
+                }
+            },
+            ... [operator_key[operator]].filter(_key => !!_key)
+                            .map(_key => ({
+                                loanBalance: {
+                                    [_key]: parseInt(amount)
+                                }
+                            }))
+        ]
+    }
+
     if (loId) {
-        data = await db.collection('groups')
-            .aggregate([
-                { $match: { loanOfficerId: loId, noOfClients: { $ne: 0 } } },
-                { $addFields: {
-                    groupIdStr: { $toString: '$_id' }
-                } },
-                {
-                    $lookup: {
-                        from: 'loans',
-                        localField: 'groupIdStr',
-                        foreignField: 'groupId',
-                        pipeline: [
-                            { $match: { $expr: { 
-                                $and: [ 
-                                    {$eq: ['$status', 'active']},
-                                    { $or: [
-                                        {$cond: {
-                                            if: { $eq: [operator, 'less_than_equal'] },
-                                            then: {$lte: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }},
-                                        {$cond: {
-                                            if: { $eq: [operator, 'greater_than_equal'] },
-                                            then: {$gte: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }},
-                                        {$cond: {
-                                            if: { $eq: [operator, 'equal'] },
-                                            then: {$eq: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }}
-                                    ] }
-                                ] 
-                            } } },
-                            { $project: {
-                                clientId: '$clientId',
-                                clientName: '$fullName',
-                                slotNo: '$slotNo',
-                                loanBalance: '$loanBalance',
-                                amountRelease: '$amountRelease',
-                                loanCycle: '$loanCycle',
-                                mcbu: '$mcbu',
-                                noOfMisPayments: '$mispayment'
-                            } },
-                            {
-                                $addFields: {
-                                    clientIdObj: { $toObjectId: '$clientId' }
-                                }
-                            },
-                            {
-                                $lookup: {
-                                    from: 'client',
-                                    localField: 'clientIdObj',
-                                    foreignField: '_id',
-                                    pipeline: [
-                                        {
-                                            $project: {
-                                                delinquent: '$delinquent'
-                                            }
-                                        }
-                                    ],
-                                    as: 'clientStatus'
-                                }
-                            }
-                        ],
-                        as: 'loans'
-                    }
+        data = await graph.apollo.query({
+            query: GROUP_QL,
+            variables: {
+                groupWhere: {
+                    loanOfficerId: { _eq: loId },
+                    noOfClients: { _neq: 0 }
                 },
-                {
-                    $project: {
-                        name: '$name',
-                        branchId: '$branchId',
-                        loans: '$loans'
-                    }
-                }
-            ]).toArray();
+                loanWhere,
+            }
+        }).then(r => r.data.groups ?? []);
+        
     }  else if (branchId) {
-        data = await db.collection('users')
-            .aggregate([
-                { $match: { "role.rep": 4, designatedBranchId: branchId } },
-                { $addFields: {
-                    loId: { $toString: '$_id' },
-                } },
-                {
-                    $lookup: {
-                        from: 'loans',
-                        localField: 'loId',
-                        foreignField: 'loId',
-                        pipeline: [
-                            { $match: { $expr: { 
-                                $and: [ 
-                                    {$eq: ['$status', 'active']},
-                                    { $or: [
-                                        {$cond: {
-                                            if: { $eq: [operator, 'less_than_equal'] },
-                                            then: {$lte: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }},
-                                        {$cond: {
-                                            if: { $eq: [operator, 'greater_than_equal'] },
-                                            then: {$gte: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }},
-                                        {$cond: {
-                                            if: { $eq: [operator, 'equal'] },
-                                            then: {$eq: ['$loanBalance', parseInt(amount)]},
-                                            else: null
-                                        }}
-                                    ] }
-                                ] 
-                            } } },
-                            { $group: {
-                                _id: '$loId',
-                                totalClients: { $sum: 1 },
-                                totalAmountRelease: { $sum: '$amountRelease' },
-                                totalLoanBalance: { $sum: '$loanBalance' },
-                                totalMCBU: { $sum: '$mcbu' }
-                            } }
-                        ],
-                        as: 'loans'
-                    }
-                },
-                {
-                    $project: {
-                        firstName: '$firstName',
-                        lastName: '$lastName',
-                        branchId: '$designatedBranchId',
-                        loans: '$loans',
-                        loNo: '$loNo'
-                    }
-                },
-                {
-                    $sort: { loNo: 1 }
-                }
-            ]).toArray();
+        const userIds = await graph.query(queryQl(createGraphType('users', `_id`)('users'), { 
+            where: { role: { _contains: { rep: 4 } } },
+            order_by: [{ loNo: 'asc' }],
+        })).then(res => res.data.users.map(u => u._id));
+
+        data = await Promise.all(userIds.map(async _id => await getQueryById(USERS_QL, branchId, loanWhere)));
     } else {
         let branchIdsObj;
         if (branchIds) {
             branchIdsObj = JSON.parse(branchIds);
         } else {
-            const branches = await db.collection('branches').find({}).toArray();
-            branchIdsObj = branches.map(branch => branch._id + '');
+            branchIdsObj = await graph.query(queryQl(createGraphType('branches', '_id')('branches'))).then(res => res.data.branches.map(b => b._id));
         }
-        
-        const branchData = [];
-        const promise = await new Promise(async (resolve) => {
-            const response = await Promise.all(branchIdsObj.map(async (branchId) => {
-                branchData.push.apply(branchData, await getByBranch(db, branchId, amount, operator));
-            }));
 
-            resolve(response);
-        });
-
-        if (promise) {
-            data = branchData;
-        }
+        data = await Promise.all(branchIdsObj.map(async _id => await getQueryById(BRANCH_QL, branchId, loanWhere)));
     }
 
     response = { success: true, data: data };
@@ -179,62 +143,21 @@ async function allLoans(req, res) {
         .end(JSON.stringify(response));
 }
 
-
-const getByBranch = async (db, branchId, amount, operator) => {
-    const ObjectId = require('mongodb').ObjectId;
-
-    return await db.collection('branches')
-        .aggregate([
-            { $match: { _id: new ObjectId(branchId) } },
-            { $addFields: {
-                branchIdStr: { $toString: '$_id' },
-            } },
-            {
-                $lookup: {
-                    from: 'loans',
-                    localField: 'branchIdStr',
-                    foreignField: 'branchId',
-                    pipeline: [
-                        { $match: { $expr: { 
-                            $and: [ 
-                                {$eq: ['$status', 'active']},
-                                { $or: [
-                                    {$cond: {
-                                        if: { $eq: [operator, 'less_than_equal'] },
-                                        then: {$lte: ['$loanBalance', parseInt(amount)]},
-                                        else: null
-                                    }},
-                                    {$cond: {
-                                        if: { $eq: [operator, 'greater_than_equal'] },
-                                        then: {$gte: ['$loanBalance', parseInt(amount)]},
-                                        else: null
-                                    }},
-                                    {$cond: {
-                                        if: { $eq: [operator, 'equal'] },
-                                        then: {$eq: ['$loanBalance', parseInt(amount)]},
-                                        else: null
-                                    }}
-                                ] }
-                            ] 
-                        } } },
-                        { $group: {
-                            _id: '$branchid',
-                            totalClients: { $sum: 1 },
-                            totalAmountRelease: { $sum: '$amountRelease' },
-                            totalLoanBalance: { $sum: '$loanBalance' },
-                            totalMCBU: { $sum: '$mcbu' }
-                        } }
-                    ],
-                    as: 'loans'
-                }
-            },
-            {
-                $project: {
-                    name: '$name',
-                    code: '$code',
-                    loans: '$loans'
-                }
+const getQueryById = async (query, _id, loanWhere) => await graph.apollo.query(
+        {
+            query,
+            variables: {
+                _id,
+                loanWhere,
             }
-        ])
-        .toArray();
-}
+        })
+          .then(res => res.data.map(u => ({
+            ... u.branch?.[0],
+            loans: u.loans.map(l => ({
+                _id,
+                totalClients: l.aggregate.totalClient,
+                totalAmountRelease: l.aggregate.totalAmountRelease,
+                totalLoanBalance: l.aggregate.totalLoanBalance,
+                totalMCBU: l.aggregate.totalMCBU
+            }))
+        })))
