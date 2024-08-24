22ecea20588233bf6f7df5ce2bffed84b19521c3
evalle012006 <evalle012006@gmail.com>
2024-08-24 21:26:03 +0800

    - redux updates

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { createWrapper } from 'next-redux-wrapper';
+import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 import rootReducer from './reducers/rootReducer';
-
-// const middleware = [thunk];
 
 const composeEnhancers =
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? 
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-const makeStore = () => createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const masterReducer = (state, action) => {
+    if (action.type === HYDRATE) {
+        return {
+            ...state,
+            ...action.payload
+        };
+    } else {
+        return rootReducer(state, action);
+    }
+};
 
-export const wrapper = createWrapper(makeStore) 
+const makeStore = () => createStore(
+    masterReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export const wrapper = createWrapper(makeStore);
