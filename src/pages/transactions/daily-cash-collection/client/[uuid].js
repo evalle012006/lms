be8ffde5e2060bb7c7bae807102d0738c3331c17
evalle012006 <evalle012006@gmail.com>
2025-04-24 19:54:19 +0800

    - fixed mcbu withdrawal for groupLeader

--- src/pages/transactions/daily-cash-collection/client/[uuid].js
+++ src/pages/transactions/daily-cash-collection/client/[uuid].js
@@ -3064,7 +3064,7 @@ const CashCollectionDetailsPage = () => {
                                                     <React.Fragment>
                                                         {(!isWeekend && !isHoliday && currentUser.role.rep > 2 && !groupSummaryIsClose) && (
                                                             <div className='flex flex-row p-2 justify-end'>
-                                                                {(data && data.length > 0) && <ActionDropDown origin="cash-collection" data={cc} index={index} options={dropDownActions} dataOptions={{ filter: filter, prevDraft: prevDraft, editMode: editMode, currentMonth: currentMonth, last5DaysOfTheMonth: last5DaysOfTheMonth }} />}
+                                                                {(data && data.length > 0) && <ActionDropDown origin="cash-collection" data={cc} index={index} options={dropDownActions} dataOptions={{ filter: filter, prevDraft: prevDraft, editMode: editMode, currentMonth: currentMonth, currentDate: currentDate, last5DaysOfTheMonth: last5DaysOfTheMonth }} />}
                                                             </div>
                                                         )}
                                                     </React.Fragment>
