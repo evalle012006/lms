07b49885e8dd9042f199617a0c80e41cdec611da
ramoscmichael <34493051+ramoscmichael@users.noreply.github.com>
2025-05-18 23:35:47 +0800

    :wip

--- src/pages/api/v2/dashboard/index.js
+++ src/pages/api/v2/dashboard/index.js
@@ -15,6 +15,56 @@ async function getData(req, res) {
 
     let { areaId, divisionId, regionId, branchId, loId, filter, date_added, type } = req.query;
 
+    if(type == 'sumarry') {
+        return await graph.apollo.query({
+            query: gql`
+            query get_dashboard_totals {
+                get_dashboard_totals(args: {
+                    currentDate: "2025-05-18",
+                    dateAdded: "2025-05-18",
+                    groupId: null,
+                    loId: null,
+                    branchId: null,
+                    areaId: null,
+                    divisionId: null,
+                    regionId: null,
+                    range: "monthly"
+                }) {
+                    data
+                }
+            }
+            `,
+            variables: {
+                args: {
+                    areaId: user.areaId ?? areaId ?? null,
+                    divisionId: user.divisionId ?? divisionId ?? null,
+                    regionId: user.regionId ?? regionId ?? null,
+                    groupId: null,
+                    loId: user.role.rep === 4 ? user._id : loId ?? null
+                }
+            }
+        }).then(res => res.data.get_dashboard_totals.map(c => c.data))
+          .then(totals => totals.map(total => ({
+            ... total,
+            clientMcbuWithdrawals: total.mcbuWithdrawal,
+            prev_clientMcbuWithdrawals: total.prev_mcbuWithdrawal,
+            pending: total.pendingClients,
+            prev_pending: total.prev_pendingClients,
+            clientMcbuReturn: total.mcbuReturn,
+            prev_clientMcbuReturn: total.prev_mcbuReturn,
+            pastDuePerson: total.pastDueNo,
+            prev_pastDuePerson: total.prev_pastDueNo,
+            mispaymentPerson: total.mispay,
+            prev_mispaymentPerson: total.prev_mispay,
+            newMember: total.currentReleasePerson_New,
+            prev_newMember: total.prev_currentReleasePerson_New,
+            fullPayment: total.fullPaymentPerson,
+            prev_fullPayment: total.prev_fullPaymentPerson,
+            amount: total.currentReleaseAmount,
+            prev_amount: total.prev_currentReleaseAmount,
+          })));
+    }
+
     const result = await graph.apollo.query({
         query: gql`
         query get_dashboard_summary_view ($args: get_dashboard_summary_view_arguments!){
