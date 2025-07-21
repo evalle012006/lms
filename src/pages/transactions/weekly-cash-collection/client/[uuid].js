e6369616bbecda626be0d05bebf834a45c10d0a2
evalle012006 <evalle012006@gmail.com>
2025-07-21 21:01:38 +0800

    fixed csf validation

--- src/pages/transactions/weekly-cash-collection/client/[uuid].js
+++ src/pages/transactions/weekly-cash-collection/client/[uuid].js
@@ -1499,7 +1499,8 @@ const CashCollectionDetailsPage = () => {
                     }
                 }
 
-                if (cc.csfError || (cc?.groupLeader && safeNumber(cc.csfCollection) <= 0)) {
+                if (cc.csfError || (cc?.groupLeader && safeNumber(cc.csfCollection) <= 0) 
+                        && (cc.remarks && (!cc.remarks.value?.startsWith('delinquent') && cc.remarks.value !== "past due" && !cc.remarks.value?.startsWith('excused')))) {
                     errorMsg.add('Error occured. Please double check the CSF Collection column.');
                 }
             } else if (cc.status !== 'totals' && (cc?.transferStr == null || cc?.transferStr == '-') && (cc.status === 'completed' || (cc?.status !== 'closed' && cc?.loanBalance <= 0)) && (!cc.remarks || (cc.remarks && (cc.remarks.value !== 'pending' && !cc.remarks.value?.startsWith('reloaner') && !cc.remarks.value?.startsWith('offset'))))) {
