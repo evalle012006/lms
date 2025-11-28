07d015e0d864eb53e672962ff2a905d158df506c
evalle012006 <evalle012006@gmail.com>
2025-11-28 10:33:26 +0800

    updates

--- src/pages/api/v2/transactions/cash-collections/update-group-transaction-status.js
+++ src/pages/api/v2/transactions/cash-collections/update-group-transaction-status.js
@@ -105,7 +105,8 @@ async function processLOApproval(loId, currentDate, currentTime, mode, transacti
             const currentCc = cc.cashCollections[0];
             const tda = currentCc ? currentCc.tda : 0;
             const goodExcused = currentCc ? currentCc.goodExcused : 0;
-            const mispayments = currentCc ? currentCc.mispayments + tda + goodExcused : 0;
+            const maturedPd = currentCc ? currentCc.maturedPd : 0;
+            const mispayments = currentCc ? currentCc.mispayments + tda + goodExcused + maturedPd : 0;
             if (cc.denom === 0 && cc.cashCollections.length > 0 
                 && currentCc.count > 0
                 && mispayments > 0
