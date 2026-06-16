10849e69253dce2d3417cc410de21cc3150ad512
evalle012006 <evalle012006@gmail.com>
2026-06-16 20:07:54 +0800

    ldf updates

--- src/pages/transactions/loan-applications/index.js
+++ src/pages/transactions/loan-applications/index.js
@@ -1871,6 +1871,7 @@ const LoanApplicationPage = () => {
                                                 <ReactToPrint
                                                     trigger={() => <ButtonSolid label="Print LDF" icon={[<PrinterIcon className="w-5 h-5" />, 'left']} width='!w-28'/> }
                                                     content={() => ndsFormRef.current }
+                                                    documentTitle={`AC_LDF_${moment(currentDate).format('YYYY-MM-DD_HH-mm-ss')}`}
                                                 />
                                             </div>
                                         )}
