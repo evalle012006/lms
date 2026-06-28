017e0bf01fccbf2e25e3bec3dcbae505c23eedd2
evalle012006 <evalle012006@gmail.com>
2026-06-29 03:22:52 +0800

    show CI to lo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -260,7 +260,7 @@ const MenuItems = [
                 active: false,
                 hasSub: false,
                 hidden: false,
-                roles: ["admin", "deputy_director", "regional_manager", "area_admin", "branch_manager"]
+                roles: ["admin", "deputy_director", "regional_manager", "area_admin", "branch_manager", "loan_officer"]
             },
             {
                 label: "Prospect Clients",
